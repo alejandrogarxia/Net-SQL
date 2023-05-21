556ab336886830b38e534ce258168fd3202cf807
alejandrogarxia <90810645+alejandrogarxia@users.noreply.github.com>
2023-05-20 18:53:41 -0600

    actualizar

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace dotnet
+{
+class Program
+{
+static void Main(string[] args)
+{
+
+try 
+            { 
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                builder.DataSource = "DESKTOP-V1BP2JO\\SQLEXPRESS"; 
+                builder.UserID = "ProgramacionWeb";            
+                builder.Password = "123456";     
+                builder.InitialCatalog = "Escuela";
+                builder.Encrypt=false;
+            
+         
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nRealizando una consulta:");
+                    Console.WriteLine("=========================================\n");
+                    
+                    connection.Open();       
+
+                    String sql = "SELECT * FROM datos";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }                    
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("\nDone. Press enter.");
+            Console.ReadLine();
+            
+
+
+}
+
+
+}
+
+}
